ae63176198e7b123d8dfcb3168c41a80a1557d94
treee <2332532718@qq.com>
2020-08-17 18:30:46 +0800

    0.2.1

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ BaseKit for project
 ### 0.1.1
 
 * 添加了加载指示器与toast
+  
+### 0.2.1
+
+* 添加了 UIImageView 的拓展
--- source/UIKit/UIView+utils.swift
+++ source/UIKit/UIView+utils.swift
@@ -8,6 +8,27 @@
 
 import UIKit
 
+public protocol Reusable {
+    static var reuseIdentifier: String { get }
+
+    var reuseIdentifier: String? { get }
+}
+
+public extension Reusable {
+    static var reuseIdentifier: String {
+        // 如果想使用保留字，需要使用反引号
+        guard let `class` = self as? AnyClass else {return "\(self)" }
+        return NSStringFromClass(`class`)
+    }
+
+    var reuseIdentifier: String? {
+        return type(of: self).reuseIdentifier
+    }
+}
+
+extension UIView: Reusable {}
+
+
 extension UIView {
     static func loadNib<T: UIView>() -> T {
         let bundle: Bundle = Bundle.init(for: self)
